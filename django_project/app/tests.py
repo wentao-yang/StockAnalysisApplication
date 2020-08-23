d28c36d567b26fdcf8b016fa9da2ad96a8829725
Wentao Yang <wentaoyangquiz@gmail.com>
2020-08-23 00:30:29 -0500

    Fixed a bug within tests.py

--- django_project/app/tests.py
+++ django_project/app/tests.py
@@ -15,20 +15,20 @@ class TestModels(TestCase):
     def test_db_1(self): # Tests number of stocks
         self.assertEqual(len(views.client.query('select * from StockPrices limit 1').raw['series'][0]['columns'][1:]), 17)
     def test_db_2(self): # Tests individual values
-        total_count = 0
+        total_count = 0 #
         contains_zero = False
+
         stock_symbols = views.client.query('select * from StockPrices limit 1').raw['series'][0]['columns'][1:]
         for i in stock_symbols:
             query_string = 'select ' + i + ' from StockPrices'
             raw_data = views.client.query(query_string).raw['series'][0]['values']
 
             for j in raw_data:
-                price = j[1]
-                total_count += price
-                if (price <= 0):
+                total_count += 1
+                if (j[1] <= 0):
                     contains_zero = True
                     break
             if (contains_zero):
                 break
-        self.assertGreater(total_count, 0)
+        self.assertEqual(total_count, 4250)
         self.assertEqual(contains_zero, False)
