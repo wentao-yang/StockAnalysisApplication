1c085f9294b9bb429777e5efc257f96616480edd
Wentao Yang <wentaoyangquiz@gmail.com>
2020-08-22 22:49:28 -0500

    Changed URLs

--- django_project/app/urls.py
+++ django_project/app/urls.py
@@ -4,6 +4,6 @@ from . import views
 # URL paths for this application, look in views.py for more information
 urlpatterns = [
     path('', views.home),
-    path('api/data/<str:symbol>/', views.data),
+    path('api/symbol=<str:symbol>/', views.data),
     path('api/symbols/', views.symbols),
 ]
